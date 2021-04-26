f1015f6d70a23503ed5b00e55d819bf8c7586d1c
caesarbriones43 <caesarbriones43@gmail.com>
2021-04-25 21:30:18 -0500

    List users added

--- src/components/pages/private/ManageUsersScreen.js
+++ src/components/pages/private/ManageUsersScreen.js
@@ -1,11 +1,64 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect, Fragment } from "react";
+import { View } from "react-native";
+import {
+  Text,
+  ListItem,
+  Avatar,
+  Icon,
+  SearchBar,
+  Header,
+} from "react-native-elements";
+import { ScrollView } from "react-native";
 
-const ManageUsersScreen = () => {
+import { quaverApi } from "../../../api/quaverApi";
+
+const ManageUsersScreen = ({ navigation: { goBack } }) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    quaverApi
+      .get("/users")
+      .then((resp) => {
+        setUsers(resp.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log(users);
+
   return (
-    <View>
-      <Text>ManageUsersScreen</Text>
-    </View>
+    <Fragment>
+      <ScrollView style={{ flexGrow: 1 }}>
+        <View>
+          <Header
+            leftComponent={
+              <Icon
+                name="arrow-back-outline"
+                type="ionicon"
+                color="#fff"
+                onPress={() => goBack()}
+              />
+            }
+            centerComponent={{ text: "Manage Users", style: { color: "#fff" } }}
+          />
+
+          <View style={{ flex: 1 }}>
+            {users.map((user) => (
+              <ListItem key={user._id} bottomDivider>
+                <Avatar source={{ uri: user.avatar }} />
+                <ListItem.Content>
+                  <ListItem.Title>{user.name}</ListItem.Title>
+                  <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+                </ListItem.Content>
+                <Icon name="chevron-forward-outline" type="ionicon" />
+              </ListItem>
+            ))}
+          </View>
+        </View>
+      </ScrollView>
+    </Fragment>
   );
 };
 
